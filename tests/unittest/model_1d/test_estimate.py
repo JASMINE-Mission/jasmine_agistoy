0394ab76f73152362169e0c9eb8b39db5630ceb4
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2024-08-08 20:47:00 +0900

    definition of estimate

--- tests/unittest/model_1d/test_estimate.py
+++ tests/unittest/model_1d/test_estimate.py
@@ -29,30 +29,30 @@ def cal():
 
 
 def test_estimate(src, exp, cal):
-    z = estimate(src, exp, cal)[:, 2]
+    z = estimate(src, exp, cal)[:, 3]
 
     assert z == approx([0.0, 1.0, 0.0, 1.0, 0.0, 1.0])
 
 
 def test_estimate_for_source(src, exp, cal):
-    z = estimate(src[0], exp, cal)[:, 2]
+    z = estimate(src[0], exp, cal)[:, 3]
 
     assert z == approx([0.0, 0.0, 0.0])
 
 
 def test_estimate_for_exposure(src, exp, cal):
-    z = estimate(src, exp[0], cal)[:, 2]
+    z = estimate(src, exp[0], cal)[:, 3]
 
     assert z == approx([0.0, 1.0])
 
 
 def test_estimate_for_calibration(src, exp, cal):
-    z = estimate(src, exp, cal[0])[:, 2]
+    z = estimate(src, exp, cal[0])[:, 3]
 
     assert z == approx([0.0, 1.0, 0.0, 1.0, 0.0, 1.0])
 
 
 def test_estimate(src, exp, cal):
-    z = estimate(src, exp, cal, noise=0.1)[:, 2]
+    z = estimate(src, exp, cal, noise=0.1)[:, 3]
 
     assert z == approx([0.0, 1.0, 0.0, 1.0, 0.0, 1.0], abs=0.3)
