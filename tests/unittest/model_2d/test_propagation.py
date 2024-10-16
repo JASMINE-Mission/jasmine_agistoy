06f212d6252148b66dc0e04876e6f8fd2c004c9e
Pau Ramos <p.ramos@unistra.fr>
2024-10-16 18:10:56 +0900

    switched to Jacobian, new attitude and added tests
    The new attitude model correctly accounts for the rotation of the reference frame from CoMRS to FoVRS.
    New tests account for different scenarios.

--- tests/unittest/model_2d/test_propagation.py
+++ tests/unittest/model_2d/test_propagation.py
@@ -3,7 +3,7 @@
 from pytest import approx, fixture
 import numpy as np
 
-from toybis.model_2d.propagation import _icrs2comrs,_comrs2fovrs
+from toybis.model_2d.propagation import _icrs2comrs,_comrs2fovrs_fromquat,_comrs2fovrs
 
 @fixture
 def ra():
@@ -33,6 +33,18 @@ def ry_at():
 def angle_at():
     return np.pi/4
 
+@fixture
+def pt_ra():
+    return 0.0
+
+@fixture
+def pt_dec():
+    return 0.0
+
+@fixture
+def pt_rot():
+    return 0
+
 
 def test__icrs2comrs(ra, dec):
     phi_c,lambda_c = _icrs2comrs(ra,dec)
@@ -41,9 +53,15 @@ def test__icrs2comrs(ra, dec):
 
     assert lambda_c == approx(0.0)
 
-def test__comrs2fovrs(phi_c,lambda_c, rx_at,ry_at,angle_at):
-    eta,zeta = _comrs2fovrs(phi_c,lambda_c, rx_at,ry_at,angle_at)
+def test__comrs2fovrs_fromquat(phi_c,lambda_c, rx_at,ry_at,angle_at):
+    eta,zeta = _comrs2fovrs_fromquat(phi_c,lambda_c, rx_at,ry_at,angle_at)
 
     if rx_at == 0 and ry_at == 0:
         assert eta == approx(phi_c-angle_at)
         assert zeta == approx(lambda_c)
+
+def test__comrs2fovrs(phi_c,lambda_c, pt_ra,pt_dec,pt_rot):
+    eta,zeta = _comrs2fovrs(phi_c,lambda_c, pt_ra,pt_dec,pt_rot)
+
+    assert eta == 0.
+    assert zeta == 0.
--- toybis/model_2d/propagation.py
+++ toybis/model_2d/propagation.py
@@ -21,7 +21,7 @@ def _icrs2comrs(ra,dec):#,parallax,pmra,pmdec,satellite_efimerides,relativistic_
     return phi_c,lambda_c
 
 
-def _comrs2fovrs(phi_c,lambda_c,rx_at,ry_at,angle_at):#,parallax,pmra,pmdec
+def _comrs2fovrs_fromquat(phi_c,lambda_c,rx_at,ry_at,angle_at):#,parallax,pmra,pmdec
     """
     This function accounts for the astrometric attitude 
     at each exposure and consists of a rotation.
@@ -73,6 +73,63 @@ def _comrs2fovrs(phi_c,lambda_c,rx_at,ry_at,angle_at):#,parallax,pmra,pmdec
     zeta = np.arcsin(u[2])
         #TO-DO: rotate proper motions as well
 
+    return eta,zeta
+
+
+def _comrs2fovrs(phi_c,lambda_c,pt_ra,pt_dec,pt_rot):
+    """
+    This function accounts for the astrometric attitude 
+    at each exposure and consists of a rotation.
+
+    Input:
+        - phi_c: longitude in CoMRS (in radiant)
+        - lambda_c: latitude in CoMRS (in radiant)
+        - pt_ra: right ascension of the centre of the FoV (in radiant)
+        - pt_dec: declination of the centre of the FoV (in radiant)
+        - pt_rot: TOTAL rotation angle, counter-clockwise (in radiant)
+
+    Output:
+        - eta: longitude in FoVRS (in radiant)
+        - zeta: latitude in FoVRS (in radiant)
+        
+        #rotation matrix
+    rot = np.array([[ca*cd,-sa*cr+ca*sd*sr,sa*sr+ca*sd*cr],
+                     [sa*cd,ca*cr+sa*sd*sr,-ca*sr+sa*sd*cr],
+                     [-sd,cd*sr,cd*cr]])
+    rot_trans = np.array([[ca*cd,sa*cd,-sd],
+                     [-sa*cr+ca*sd*sr,ca*cr+sa*sd*sr,cd*sr],
+                     [sa*sr+ca*sd*cr,-ca*sr+sa*sd*cr,cd*cr]])
+    
+        #vector to rotate (in CoMRS)
+    v = np.array([np.cos(phi_c)*np.cos(lambda_c),
+                  np.sin(phi_c)*np.cos(lambda_c),
+                  np.sin(lambda_c)])
+    """
+    #trigonometry
+    ca = np.cos(pt_ra)
+    sa = np.sin(pt_ra)
+    cd = np.cos(-pt_dec)
+    sd = np.sin(-pt_dec)
+    cr = np.cos(pt_rot)
+    sr = np.sin(pt_rot)
+    cp = np.cos(phi_c)
+    sp = np.sin(phi_c)
+    cl = np.cos(lambda_c)
+    sl = np.sin(lambda_c)
+    #rotated vector
+    u0 = cp*cl*ca*cd + sp*sl*sa*cd - sl*sd
+    u1 = cp*cl*(ca*sd*sr-sa*cr) + sp*sl*(ca*cr+sa*sd*sr) + sl*cd*sr
+    u2 = cp*cl*(sa*sr+ca*sd*cr) + sp*sl*(sa*sd*cr-ca*sr) + sl*cd*cr
+
+    #obtain new angles
+        #NOTE: we can safely use arctan2 like this because 
+        # latitude is always between -Pi/2 and Pi/2 and
+        # thus its cosine is always positive
+        #NOTE2: this will return angles between -Pi and Pi
+    eta = np.arctan2(u1,u0)
+    zeta = np.arcsin(u2)
+
+        #TO-DO: rotate proper motions as well
     return eta,zeta
 
 #TO-DO: define _fovrs2fprs (gnomonic projection) and _fprs2drs (image deformation)
