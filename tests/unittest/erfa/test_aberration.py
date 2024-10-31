6da5a29aa3826eb998151dc1839b89d7b9599229
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2024-10-31 16:34:12 +0900

    imlement aberraion function

---
+++ tests/unittest/erfa/test_aberration.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pytest import approx, fixture
+import numpy as np
+import jax.numpy as jnp
+
+from toybis.erfa.aberration import aberration
+from toybis.erfa.constants import ERFA_SRS
+
+@fixture
+def source():
+    return jnp.array([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1],
+    ])
+
+
+def norm(array, axis=1):
+    return np.sqrt(np.sum(array**2, axis=axis))
+
+
+def shift(s, v):
+    return s - np.acos((v + np.cos(s)) / (1 - v * np.cos(s)))
+
+
+def check_unitary(array, axis=1):
+    return approx(np.sum(array**2, axis=axis) - 1) == 0
+
+
+def test_aberration_direction(source):
+
+    velocity = jnp.array([0, 0, 1e-8])
+    solar_distance = 1.0
+    lorenz_bm1 = np.sqrt(1.0 - (velocity ** 2).sum())
+
+    p = aberration(source, velocity, solar_distance, lorenz_bm1)
+
+    delta = p - source
+    assert check_unitary(p)         # p should be a unit vector
+    assert approx(delta[0]) != 0    # x-direction is affected
+    assert approx(delta[1]) != 0    # y-direction is affected
+    assert approx(delta[2]) == 0    # z-direction is not affected
+
+
+def test_aberration_magnitude():
+
+    source = jnp.array([[0, 0, 1]])
+    velocity = jnp.array([
+        [0.01, 0, 0],
+        [0.05, 0, 0],
+        [0.25, 0, 0],
+        [0, 0.01, 0],
+        [0, 0.05, 0],
+        [0, 0.25, 0],
+    ])
+    solar_distance = 1.0
+    lorenz_bm1 = np.sqrt(1.0 - (velocity ** 2).sum(axis=1))
+
+    for v, l in zip(velocity, lorenz_bm1):
+        p = aberration(source, v, solar_distance, l)
+
+        # calculate angular shifts along with x- and y-axes
+        tx = np.atan2(p[0, 0], p[0, 2])
+        ty = np.atan2(p[0, 1], p[0, 2])
+
+        # calculate the expected angular shifts
+        sx = shift(np.pi / 2.0, v[0] * (1.0 + ERFA_SRS))
+        sy = shift(np.pi / 2.0, v[1] * (1.0 + ERFA_SRS))
+
+        # evaluate the differences in units of μas
+        delta_x = (tx - sx) / np.pi * (180 * 3600e6)
+        delta_y = (ty - sy) / np.pi * (180 * 3600e6)
+
+        assert check_unitary(p)     # p should be a unit vector
+        assert delta_x < 0.01       # delta_x smaller than 0.01 μas
+        assert delta_y < 0.01       # delta_y smaller than 0.01 μas
---
+++ toybis/erfa/constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+    Define the constants used in the ERFA library
+'''
+
+__all__ = [
+  'ERFA_DJ00',
+  'ERFA_DJM0',
+]
+
+
+# Reference epoch (J2000.0), Julian Date
+ERFA_DJ00 = 2451545.0
+
+# Julian Date of Modified Julian Date zero
+ERFA_DJM0 = 2400000.5
+
+# Reference epoch (J2000.0), Modified Julian Date
+ERFA_DJM00 = ERFA_DJ00 - ERFA_DJM0
+
+# Schwarzschild radius of the Sun (au)
+ERFA_SRS = 1.97412574336e-8
