9d9676fb629af1c3692663d8c7a773fa9948b6fb
Ryou Ohsawa <ryou.ohsawa@nao.ac.jp>
2024-10-31 17:28:17 +0900

    implement test for deflection

--- toybis/erfa/deflection.py
+++ toybis/erfa/deflection.py
@@ -46,7 +46,7 @@ def deflection(mass, p_source, q_source, q_observer, distance,
     '''
     q_observer = q_observer.reshape((1, 3))
 
-    qdqpe = q_source @ (q_source + q_observer).T
+    qdqpe = (q_source * (q_source + q_observer)).sum(axis=1, keepdims=True)
     w = mass * c.ERFA_SRS / distance / jnp.clip(qdqpe, min=limiter)
 
     eq = jnp.cross(q_observer, q_source)
@@ -54,7 +54,9 @@ def deflection(mass, p_source, q_source, q_observer, distance,
 
     p_natural = p_source + w * peq
 
-    return p_natural
+    norm = jnp.sqrt(jnp.sum(p_natural**2, axis=1, keepdims=True))
+
+    return p_natural / norm
 
 
 def deflection_by_sun(p_source, q_observer, distance):
@@ -70,6 +72,8 @@ def deflection_by_sun(p_source, q_observer, distance):
 
     limiter = 1e-6 / np.clip(distance**2, min=1.0)
 
-    #
+    # approximate p_source == q_source
     p_natural = deflection(
         1.0, p_source, p_source, q_observer, distance, limiter=limiter)
+
+    return p_natural
